592ca80ea1451c3be2382d795710e5e57d6332f5
HunterZ <108939+HunterZ@users.noreply.github.com>
2024-02-18 16:25:18 -0800

    CI/CD: Make a silly attempt to add MSVC build"
    - .github/workflows/rustLaunchSite.yml:
      - add x64-windows-static-md triplet base to windows-latest matrix
      - use MSYS2 MSYSTEM specified by matrix
      - only add MinGW64 to PATH when using it
      - enable release build stripping during install step (replaced previous GCC-specific CMakeLists.txt solution)
    - CMakeLists.txt:
      - comment out release build stripping, as it was GCC/Clang specific
    - CMakePresets.json:
      - add MSVC presets
    - vcpkg-triplets/*.cmake:
      - fix relative path to vcpkg install (not sure why this was working after submodule removal)
      - add MSVC overlay triplets

--- vcpkg-triplets/x64-mingw-static-minsizerel.cmake
+++ vcpkg-triplets/x64-mingw-static-minsizerel.cmake
@@ -1,2 +1,2 @@
-include("../vcpkg/triplets/community/x64-mingw-static.cmake")
+include("${VCPKG_ROOT_DIR}/triplets/community/x64-mingw-static.cmake")
 set(VCPKG_BUILD_TYPE release)
---
+++ vcpkg-triplets/x64-windows-static-md-release.cmake
@@ -0,0 +1,2 @@
+include("${VCPKG_ROOT_DIR}/triplets/community/x64-windows-static-md.cmake")
+set(VCPKG_BUILD_TYPE release)
