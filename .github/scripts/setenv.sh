42c3c2beee1f17de0fd9aec1653340a2a6ee25e7
HunterZ <108939+HunterZ@users.noreply.github.com>
2024-02-22 23:32:12 -0800

    CI/CD: don't override things, and try using builtin MSYS2 on runner

--- .github/scripts/setenv.sh
+++ .github/scripts/setenv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Setup ${MSYSTEM} PATH"
-MSYS_BIN=$(echo "${RUNNER_TEMP}/msys64${MSYSTEM_PREFIX}/bin" | sed 's|\\|/|g')
-export PATH="${MSYS_BIN}:${PATH}"
+# MSYS_BIN=$(echo "${RUNNER_TEMP}/msys64${MSYSTEM_PREFIX}/bin" | sed 's|\\|/|g')
+# export PATH="${MSYS_BIN}:${PATH}"
 echo "Updated path: ${PATH}"
 
 # echo "Setup vcpkg root"
