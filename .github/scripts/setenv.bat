9a387e9431187efe6b8a7865ddc6f63609106975
HunterZ <108939+HunterZ@users.noreply.github.com>
2024-02-22 19:27:53 -0800

    CI/CD: try going back to raw vcpkg, this time from a clone instead of submodule

--- .github/scripts/setenv.bat
+++ .github/scripts/setenv.bat
@@ -2,3 +2,6 @@
 echo Setup Chocolatey PATH
 set PATH=%ChocolateyInstall%\bin;%PATH%
 echo Updated PATH: %PATH%
+
+echo Setup vcpkg root
+set VCPKG_ROOT=%GITHUB_WORKSPACE%\vcpkg
--- .github/scripts/setenv.sh
+++ .github/scripts/setenv.sh
@@ -2,3 +2,6 @@
 echo "Setup ${MSYSTEM} PATH"
 export PATH="${RUNNER_TEMP}/msys64${MSYSTEM_PREFIX}/bin:${PATH}"
 echo "Updated path: ${PATH}"
+
+echo "Setup vcpkg root"
+export VCPKG_ROOT="${GITHUB_WORKSPACE}/vcpkg"
